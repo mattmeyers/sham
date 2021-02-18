5e72235acf319726e87716fc0e23c8bf4a26b98a
Matt Meyers <matt@mattmeyers.dev>
2021-02-17 23:25:53 -0500

    Introduce terminal generator adapters
    In order to strongly type the return type terminal generators, adapter
    functions can be used to convert the function signature back to the
    GeneratorFunc type. This provides a nicer interface for interacting
    with the terminal generators as a library.

--- generator.go
+++ generator.go
@@ -13,28 +13,31 @@ type GeneratorFunc func() interface{}
 
 func (f GeneratorFunc) Generate() interface{} { return f() }
 
+func stringAdaptor(f func() string) func() interface{} { return func() interface{} { return f() } }
+func intAdaptor(f func() int) func() interface{}       { return func() interface{} { return f() } }
+
 var TerminalGenerators = map[string]Generator{
-	"name":        GeneratorFunc(Name),
-	"firstName":   GeneratorFunc(FirstName),
-	"lastName":    GeneratorFunc(LastName),
-	"phoneNumber": GeneratorFunc(PhoneNumber),
+	"name":        GeneratorFunc(stringAdaptor(Name)),
+	"firstName":   GeneratorFunc(stringAdaptor(FirstName)),
+	"lastName":    GeneratorFunc(stringAdaptor(LastName)),
+	"phoneNumber": GeneratorFunc(stringAdaptor(PhoneNumber)),
 }
 
 func getRandomString(vals []string) string { return vals[rand.Intn(len(vals))] }
 
-func Name() interface{} {
+func Name() string {
 	return getRandomString(firstNames) + " " + getRandomString(lastNames)
 }
 
-func FirstName() interface{} {
+func FirstName() string {
 	return getRandomString(firstNames)
 }
 
-func LastName() interface{} {
+func LastName() string {
 	return getRandomString(lastNames)
 }
 
-func PhoneNumber() interface{} {
+func PhoneNumber() string {
 	const digits string = "1234567890"
 	var sb strings.Builder
 
